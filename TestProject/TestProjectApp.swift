76ad6664622ed4cbd173202edcb09d310ddcf3c1
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2022-12-19 16:33:01 +0700

    test commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Rifqi Muhammad Aziz on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
